ddcc73b37f5873e22bde8b678f849bb2d21b67dd
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2021-10-08 00:15:45 -0600

    Yelp Challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+
+  Review.create!(
+    rating: rand(1..5),
+    content: Faker::ChuckNorris.fact,
+    restaurant: restaurant
+  )
+end
